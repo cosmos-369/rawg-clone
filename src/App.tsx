4c2e1665d42eee62c7d657715ee9ad9d61e6a747
cosmos-369 <cosmos333999@gmail.com>
2023-11-13 19:44:56 +0530

    add navbar and logo placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg={"violet"}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg={"orange"}>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
